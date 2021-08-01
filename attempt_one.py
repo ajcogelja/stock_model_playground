199ca756268de0821000aee6b26f7f3f3ba8fc59
ajcogelja <acogelj@purdue.edu>
2021-07-31 21:09:26 -0500

    better model

--- attempt_one.py
+++ attempt_one.py
@@ -8,7 +8,7 @@ def main():
      data = pd.read_csv('all_stocks_5yr.csv')
      tickers = data.Name.unique()
      #print('tickers:', tickers)
-     split = split_by_tickers(data, tickers)
+     #split = split_by_tickers(data, tickers)
      #print(split)
      ticker_index = 0
      means = []
@@ -16,44 +16,89 @@ def main():
      largest_mean_open_close_diff = -float('inf')
      largest_high_low = -float('inf')
      high_low_label = ''
-     #for s in split:
-         #mean = s.mean()
-         #diff = mean['close'] - mean['open']
-         #high_low = mean['high'] - mean['low']
-         #if high_low > largest_high_low:
-         #    largest_high_low = high_low
-         #    high_low_label = tickers[ticker_index]
-         #if diff > largest_mean_open_close_diff:
-          #   largest_mean_open_close_diff = diff
-           #  largest_mean_open_close_diff_label = tickers[ticker_index]
-
-         #print(mean['high'], 'close - open')
-         #means.append(mean)
-         #print(tickers[ticker_index], '\n ', mean)
-         #ticker_index += 1
 
      #print('largest high_low: ', largest_high_low, 'label: ', high_low_label)
      #print('largest one day change:', largest_mean_open_close_diff_label, ' :', largest_mean_open_close_diff)
      #constant_set, error_log, min_error, min_constants, min_month
-     model, error_log, min_error, min_constants, min_month = build_model(data, 1, 1)
+     trail_length = 8
+     ticker_num = 0
+     data = data[data['Name'] == tickers[ticker_num]]
+     #data = data[data['date'].dt.year == 2013]
+     model, bias, error_log, min_error, min_constants, min_month = build_model(data, 1, 3, trail_length)
      print('model: ', model)
      print('min average error:', min_error)
      print('min_constants: ', min_constants)
+     print('min_bias: ', bias)
      print('min_month', min_month)
 
-     meanError, predictions, actualList = testModel(min_constants, data, 10, 'close')
+     print('ticker used: ', 'AAL')
+     #data = data[data['date'].dt.year == 2013]
+
+     meanError, predictions, actualList = testModel(min_constants, bias, data, trail_length, 'close', min_month)
      axis = []
      for i in range(len(predictions)):
          axis.append(i)
      print('mean error: ', meanError)
-     print('actual: ', actualList)
-     print('predictions: ', predictions)
-     plt.plot(axis, predictions)
-     plt.plot(axis, actualList)
+     #print('actual: ', actualList)
+     #print('predictions: ', predictions)
+     starting_wallet = 10000
+     print('starting wallet: ', starting_wallet)
+     print('Slope Sign Accuracy, netVal, wallet: ', slopeSignAccuracy(actualList, predictions, starting_wallet=starting_wallet))
+     print('netval is calculated by assuming our predicgtion about a price increase is correct we buy today sell tomorrow')
+     plt.plot(axis, predictions, color="green")
+     plt.plot(axis, actualList, color="orange")
      plt.show()
 
 
      #print('error_log: ', error_log)
+
+def slopeSignAccuracy(data, predictions, starting_wallet):
+    size = 0
+    matches = 0
+    buy = False #buy on first iteration no matter what
+    sell = False
+    purchasePower = 10
+    wallet = starting_wallet
+    shares = {
+        'amount': 0,
+        'pps': 0
+    }
+    netVal = 0
+    for i in range(len(data) - 2):
+        if shares['amount'] != 0:
+            wallet += shares['amount']*(data[i]) #gah this is wrong anyways cause of tickers
+            shares['amount'] = 0 #would work if our model ran for an individual stock... but it doesnt
+            shares['pps'] = 0
+        if buy: #if we think price goes up tomorrow we buy and then when we sell tomorrow we change by price_tomorrow - price_today
+            netVal += data[i + 1] - data[i]
+            shares['amount'] = int(wallet/data[i])
+            print('buying: ', shares['amount'], ' shares at: ', data[i])
+            wallet -= shares['amount']*data[i] #subtract cost
+            shares['pps'] = data[i] #save the price
+        
+        dataSlope = data[i + 1] - data[i]
+        predSlope = predictions[i + 1] - predictions[i]
+        if predSlope > 0:
+            sell = True
+            buy = False
+        elif predSlope < 0:
+            buy = True
+            sell = False
+        if predSlope > 0 and dataSlope > 0:
+            matches = matches + 1
+        if predSlope < 0 and dataSlope < 0:
+            matches = matches + 1
+        if predSlope == 0 and dataSlope == 0:
+            matches = matches + 1
+        size = size + 1
+
+    if shares['amount'] != 0:
+            wallet += shares['amount']*(data[i]) #gah this is wrong anyways cause of tickers
+            shares['amount'] = 0 #would work if our model ran for an individual stock... but it doesnt
+            shares['pps'] = 0
+
+    return float(matches)/size, netVal, wallet
+
 def approximationFunction(degree, constants, value):
     sum = 0
     for i in range(degree):
@@ -61,22 +106,23 @@ def approximationFunction(degree, constants, value):
     
     return sum
 
-def testModel(model, testData, trail_length, attribute):
+def testModel(model, bias, testData, trail_length, attribute, month):
     errorSum = 0
     counter = 0
     predictions = []
     actualList = []
+    testData = testData[testData['date'].dt.month == month]
     output = testData[attribute]
-    print('test data index: ', output.index)
+    print('test data for month', month)
     for data in range(len(output.index) - 4*trail_length):
         vals = []
         for i in range(trail_length):
             #print('index index ', data + i, 'index: ', testData.index[data + i])
-            val = output[output.index[data + i] + 1]
+            val = output[output.index[data + i + 1]]
             vals.append(val)
             
-        prediction = predict(vals, model)
-        actual = output[output.index[data + trail_length + 1]]
+        prediction = predict(vals, model, bias)
+        actual = output[output.index[data + trail_length]]
         error = calcError(prediction, actual)
         errorSum = errorSum + abs(error)
         counter = counter + 1
@@ -94,7 +140,7 @@ def getClosingPrices(data, month):
     return closing
     
     #bad model, might have to do this differently
-def build_model(data, iterations, steps):
+def build_model(data, iterations, steps, trail_length):
     #what kind of model do i wanna do tho??
     #heres what im thinking, we plot the close prices and try fitting different degree exponential functions to the curve to minimize error
     #first x^0 , then k_0 * 1 + k_1 * x, then k_0 + k_1 * x + k_2 * x_2
@@ -102,17 +148,19 @@ def build_model(data, iterations, steps):
         #get function error across data set
     
     constant_set = []#np.random.random_integers(10, size=(1, 1))
-    trail_length = 10
+    #trail_length = 10
     error_log = []
     min_error = float('inf')
     min_constants = []
     min_month = []
+    min_bias = 0
     for i in range(iterations):
         #constants.append(random.randint(-3, 3))
         for month in range(1, 12):
             constants = []
             errorSum = 0
             length = 0
+            bias = random.random()
             for t in range(trail_length):
                 constants.append(random.random())#1
                 closing = getClosingPrices(data, month)
@@ -128,14 +176,15 @@ def build_model(data, iterations, steps):
                         #go from i to i + j
                         #print('i + j: ',  (start + i + j), ' : ', closing)
                         vals.append(closing[closing.index[i + j]])
-                    prediction = predict(vals, constants)
+                    prediction = predict(vals, constants, bias)
                     #print('start + i + length: ', closing.index[i + trail_length])
                     error = calcError(prediction, closing[closing.index[i + trail_length]])
                     errorSum = errorSum + abs(error)
                     #print('error: ', error)
                     for w in range(len(constants)):
-                        w_prime = constants[w] + error/(len(constants)*max(abs(constants[w]), 1))
+                        w_prime = constants[w] + error/(pow(len(constants), 2)*max(abs(constants[w]), 1))
                         constants[w] = w_prime
+                    bias = bias + (error/pow(len(constants), 3))
             
             errorSum = errorSum/length
 
@@ -143,10 +192,11 @@ def build_model(data, iterations, steps):
                 min_error = errorSum
                 min_constants = constants
                 min_month = month
+                min_bias = bias
             constant_set.append(constants)   
                 
     #print('error_log:', error_log)
-    return constant_set, error_log, min_error, min_constants, min_month
+    return constant_set, min_bias, error_log, min_error, min_constants, min_month
 
 def calcError(pred, actual):
     #print('pred: ', pred, ' actual: ', actual)
@@ -158,8 +208,8 @@ def calcError(pred, actual):
         return error
 
     
-def predict(vals, constants):
-    prediction = 0
+def predict(vals, constants, bias):
+    prediction = bias
     for i in range(len(vals)):
         prediction += constants[i]*vals[i]
     return prediction

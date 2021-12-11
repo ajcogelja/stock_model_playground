8acaa35c8c9df6e6f46103ae762fbdde735dda69
Alex Cogelja <ajcogelja@gmail.com>
2021-12-11 12:15:35 -0600

    variety of new partially started projects

---
+++ competitive_percepton.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+import random
+import math
+import matplotlib.pyplot as plt
+
+def main():
+    #read data
+    data = pd.read_csv('heart.csv')
+    #chunk data into two sections 80-20% training-validation data
+        #chunk 80% into 2 parts: 60-20%
+            #20 is used for scoring offspring before pairing
+
+    #generate n random models (perhaps) train each model to get to 100% on a particular line of the dataset
+
+    #score each
+
+    #pair off and reproduce
+
+    #test offspring
+
+    #repeat k times
+

fd3f3e902c3f9320388bb5519776dddc4487b769
Alex Cogelja <ajcogelja@gmail.com>
2021-12-14 21:52:56 -0600

    started implementing model

--- me_bot_2_0.py
+++ me_bot_2_0.py
@@ -6,7 +6,35 @@ import matplotlib.pyplot as plt
 
 def main():
     print("Hello and welcome to the M.e Bot ai program trained!")
-   
+
+suffix_freq = {}
+prefix_freq = {}
+
+
+def process_word(word, last_word, sum):
+    #gen word vector based off word and last_word
+    
+    #detect_pref
+    word_len = len(word)
+    for i in range(word_len):
+        suffix = ''
+        prefix = ''
+        if max((word_len/2 - 1), 0) < word_len - 1 - i:
+            suffix += word[i]
+            if suffix_freq.has_key(suffix):
+                curr = suffix_freq[suffix]
+                suffix_freq[suffix] = (curr + 1)
+            else:
+                suffix_freq[suffix] = 1
+        if i < min((word_len/2 + 1), word_len) :
+            prefix += word[i]
+            if prefix_freq.has_key(prefix):
+                curr = prefix_freq[prefix]
+                prefix_freq[prefix] = (curr + 1)
+            else:
+                prefix_freq[prefix] = 1
+
+
     
 
 

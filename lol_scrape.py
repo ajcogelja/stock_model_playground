964b75b53c80c39b3164fe26499fe2777c0d9fd8
ajcogelja <acogelj@purdue.edu>
2021-12-30 03:24:36 -0600

    generate output json

--- lol_scrape.py
+++ lol_scrape.py
@@ -6,6 +6,7 @@ import requests as req
 import time
 import random
 import re
+import json
 
 headers = {
     'referer': 'https://na.op.gg/',
@@ -14,8 +15,13 @@ headers = {
 html_doc = req.get('https://na.op.gg/champion/statistics', headers=headers)
 soup = BeautifulSoup(html_doc.content, 'html.parser')
 
+def find_tier(page):
+    found = page.find(lambda tag : (tag.name == 'b' and tag.text and 'Tier ' in tag.text))
+    #print('find:', found.text.replace('Tier ', ''))
+    return int(found.text.replace('Tier ', ''))
+
 def get_counter_champs(counter_url):
-    print(counter_url)
+    #print(counter_url)
     req_headers = {
         'referer': 'https://na.op.gg/',
         'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36'
@@ -27,6 +33,8 @@ def get_counter_champs(counter_url):
         'data-champion-name': True, 
         'data-value-winrate' : True,
     }
+    tier = find_tier(champ_page.body)
+    print('tier for role: ', tier)
     counter_list = champ_page.find_all('div', attrs=attributes)
     counters = {} #dict
     for counter in counter_list:
@@ -39,7 +47,7 @@ def get_counter_champs(counter_url):
             'pickrate': float(game_div.span.text.replace('%', '')),
             'games': int(c_attrs['data-value-totalplayed'])
         }
-    return counters
+    return tier, counters
 
 def contains_id_indicator(tag: Tag):
     id_str = 'championId:'
@@ -70,11 +78,6 @@ def find_champ_id(page : Union[Tag, BeautifulSoup]):
 
     return -1
 
-def find_tier(page):
-    found = page.find(lambda tag : (tag.name == 'b' and tag.text and 'Tier ' in tag.text))
-    #print('find:', found.text.replace('Tier ', ''))
-    return int(found.text.replace('Tier ', ''))
-
 def get_champ_info(url, name):
     req_headers = {
         'referer': 'https://na.op.gg/',
@@ -84,18 +87,37 @@ def get_champ_info(url, name):
     champ_page = BeautifulSoup(champ_page_req.content, 'html.parser')
     champ_page_body = champ_page.body
     info = {}
-    #print('regex:', champ_page(text=re.compile('counters'))) -- exaple on string parsing
+    #print('regex:', champ_page(text=re.compile('counters'))) -- example on string parsing
     info['id'] = find_champ_id(champ_page_body)
-    info['tier'] = find_tier(champ_page_body)
-    found_anchors = champ_page_body.find_all('a', attrs={'href': True}, text='Counters')
-    for a in found_anchors:
-        print('a', a)
-        print('a.attrs: ', a.attrs)
-        print('a.text: ', a.text)
-        counter_url = 'https://na.op.gg' + a.attrs['href']
-        counters = get_counter_champs(counter_url)
-        info['counters'] = counters
+    #/champion/aatrox/statistics/top #list with data position attribute link
+    #/champion/aatrox/statistics/top/matchup #what i currently have
+    # found_anchors = champ_page_body.find_all('a', attrs={'href': True}, text='Counters')
+    #I could add a roles thing in info, or i could
+    counters = {}
+    roles = champ_page_body.find_all('li', attrs={'data-position': True})
+    champ_roles = []
+    for r in roles:
+        print('r: ', r)
+        role = r.attrs['data-position'].lower()
+        print('r.data-position: ', role)
+        role_rate = r.find('span', attrs={'class' :'champion-stats-header__position__rate'})
+        print('tuple: ', (role, role_rate.text))
 
+        role_counter_url = 'https://na.op.gg/champion/' + name + '/statistics/' + role + '/matchup'
+        role_tier, role_counters = get_counter_champs(role_counter_url)
+        champ_roles.append((role, float(role_rate.text.replace('%', '')), role_tier))
+        print('role url: ', role_counter_url)
+        role_counters = {} #gen_counter_champs(role_counter_url)
+        # print('a', a)
+        # print('a.attrs: ', a.attrs)
+        # print('a.text: ', a.text)
+        # counter_url = 'https://na.op.gg' + a.attrs['href']
+        #counters = get_counter_champs(counter_url)
+        #info['counters'] = counters
+        counters[role] = role_counters
+    info['counters'] = counters
+    info['roles'] = champ_roles
+    #info['tier'] = find_tier(champ_page_body) multiple tiers dependenind on role
 
     return info
 
@@ -106,7 +128,7 @@ def main():
     champs = {}
 
     for r in res:
-        if index == 4:
+        if index == 3:
             break
         #print('index: ', index)
         champ_info = {}
@@ -118,11 +140,13 @@ def main():
             champs[name] = champ_info
             input('press a key to continue')
             #time.sleep(1  + random.random()/2) #sleep for half a second as to not get flagged as a scraping tool
-        else:
-            print('r does not have attr a')
+        #else:
+            #print('r does not have attr a')
         
         index += 1
-    print('champs: ', champs)
+    #print('champs: ', champs)
+    with open("champs.json", "w") as outfile:
+        json.dump(champs, outfile)
 
 
 if __name__ == "__main__":
